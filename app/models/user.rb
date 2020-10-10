17614d2c96e5fc0140d557a275e26f7434895e9d
liz539z <abc11nu@gmail.com>
2020-10-10 19:20:40 +0900

    デバイスUserモデル作成とマイグレーションファイル編集

---
+++ app/models/user.rb
@@ -0,0 +1,17 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  validates :email, uniqueness: true
+  validates :password, format: { with:/\A[a-zA-Z0-9]+\z/, message: "Password Include both letters and numbers" }
+  validates :nickname, presence: true
+  validates :birthday, presence: true
+  validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "First name Full-width characters" }
+  validates :last_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "Last name Full-width characters" }
+  validates :first_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: "First name kana Full-width katakana characters" }
+  validates :last_name_kana,  presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: "Last name kana Full-width katakana characters" }
+
+
+end
