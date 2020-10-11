935b062f981f6faaaeb0e7e9e51cd68728800ea7
liz539z <abc11nu@gmail.com>
2020-10-11 12:02:02 +0900

    FactoryBot作成

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,13 @@
 FactoryBot.define do
   factory :user do
-    
+    nickname {Faker::Name.unique.name}
+    email {Faker::Internet.free_email}
+    password {"a12345"}
+    password_confirmation {"a12345"}
+    first_name {"沼田"}
+    last_name {"太郎"}
+    first_name_kana {"ヌマタ"}
+    last_name_kana {"タロウ"}
+    birthday { "1930-01-01" }
   end
 end
