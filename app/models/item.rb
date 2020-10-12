e666653f01e2d68fd6de9706f4f48c2822956854
liz539z <abc11nu@gmail.com>
2020-10-12 18:24:25 +0900

    エラーメッセージ変更

--- app/models/item.rb
+++ app/models/item.rb
@@ -8,13 +8,21 @@ class Item < ApplicationRecord
   belongs_to :user
   has_one_attached :image
   with_options presence: true do
-    validates :price, format: {with: /\A[0-9]+\z/, message: "is invalid. Input half-width characters."}
+    validates :image 
+    validates :name
+    validates :describe
+    validates :price, format: {with: /\A[0-9]+\z/, message: "Half-width number"}
+    validates :category_id, numericality: { other_than: 1, message: " Select" } 
+    validates :condition_id, numericality: { other_than: 1, messages: "Select"} 
+    validates :postage_id, numericality: { other_than: 1, messages: "Select"} 
+    validates :prefecture_id, numericality: { other_than: 1, messages: "Select"} 
+    validates :delivery_day_id, numericality: { other_than: 1, messages: "Select"}
   end
-  validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999, message: "is out of setting range"}
-  validates :category_id, numericality: { other_than: 1 } 
-  validates :condition_id, numericality: { other_than: 1 } 
-  validates :postage_id, numericality: { other_than: 1 } 
-  validates :prefecture_id, numericality: { other_than: 1 } 
-  validates :delivery_day_id, numericality: { other_than: 1 } 
+  validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999, message: "Out of setting range"}
+
+
+
+
+
 
 end
