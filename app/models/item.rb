18e5202d564422c3436dca7095255b23757b91ed
liz539z <abc11nu@gmail.com>
2020-10-12 18:04:55 +0900

    モデルの作成（ActiveHash作成・User追記・Item作成）

---
+++ app/models/item.rb
@@ -0,0 +1,20 @@
+class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :category
+  belongs_to_active_hash :condition
+  belongs_to_active_hash :postage
+  belongs_to_active_hash :prefecture
+  belongs_to_active_hash :delivery_day
+  belongs_to :user
+  has_one_attached :image
+  with_options presence: true do
+    validates :price, format: {with: /\A[0-9]+\z/, message: "is invalid. Input half-width characters."}
+  end
+  validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999, message: "is out of setting range"}
+  validates :category_id, numericality: { other_than: 1 } 
+  validates :condition_id, numericality: { other_than: 1 } 
+  validates :postage_id, numericality: { other_than: 1 } 
+  validates :prefecture_id, numericality: { other_than: 1 } 
+  validates :delivery_day_id, numericality: { other_than: 1 } 
+
+end
