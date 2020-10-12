10587e2444c14207fbddd24ae91e3ee728543a83
liz539z <abc11nu@gmail.com>
2020-10-12 18:11:42 +0900

    item コントローラー編集　＋new, create

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,27 @@
 class ItemsController < ApplicationController
   def index
   end
+
+  def new
+    unless user_signed_in?
+      redirect_to new_user_session_path
+    end
+    @item = Item.new
+  end
+
+
+  def create
+    @item = Item.create(item_params)
+    if @item.save
+      redirect_to root_path (current_user.id)
+    else
+      render action: :new
+    end
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:name, :describe, :category_id, :condition_id, :postage_id, :prefecture_id, :delivery_day_id, :price, :image).merge(user_id: current_user.id)
+  end
 end
