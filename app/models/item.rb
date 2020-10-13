55a5d52437098870e760747790eb075fbe75911f
liz539z <abc11nu@gmail.com>
2020-10-13 12:21:23 +0900

    Itemモデル　with_optionsでバリデーションのまとまりを作る

--- app/models/item.rb
+++ app/models/item.rb
@@ -7,17 +7,20 @@ class Item < ApplicationRecord
   belongs_to_active_hash :delivery_day
   belongs_to :user
   has_one_attached :image
+  
   with_options presence: true do
     validates :image
     validates :name
     validates :describe
     validates :price, format: { with: /[0-9]\d/, message: 'Half-width number' }
-    validates :category_id, numericality: { other_than: 1, message: ' Select' }
-    validates :condition_id, numericality: { other_than: 1, message: 'Select' }
-    validates :postage_id, numericality: { other_than: 1, message: 'Select' }
-    validates :prefecture_id, numericality: { other_than: 1, message: 'Select' }
-    validates :delivery_day_id, numericality: { other_than: 1, message: 'Select' }
+  
+    with_options numericality: { other_than: 1, message: 'Select' } do
+      validates :category_id
+      validates :condition_id
+      validates :postage_id
+      validates :prefecture_id
+      validates :delivery_day_id
+    end
   end
-
   validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999, message: 'Out of setting range' }
 end
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -31,7 +31,7 @@ RSpec.describe Item, type: :model do
     it 'カテゴリーが選択されていない時保存できない' do
       @item.category_id = 1
       @item.valid?
-      expect(@item.errors.full_messages).to include('Category  Select')
+      expect(@item.errors.full_messages).to include('Category Select')
     end
 
     it '商品の状態が選択されていない時保存できない' do
