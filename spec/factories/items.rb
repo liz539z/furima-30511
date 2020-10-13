6d271a76ddb83e6e97e352406580bcdbfae7e991
liz539z <abc11nu@gmail.com>
2020-10-13 11:27:27 +0900

    Itemモデル　FactoryBot作成

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,20 @@
 FactoryBot.define do
   factory :item do
-    
+
+    name {"参考書"}
+    describe {Faker::Lorem.sentence}
+    category_id {2}
+    condition_id {2}
+    postage_id {2}
+    prefecture_id {2}
+    delivery_day_id {2}
+    price {2000}
+
+    association :user
+
+    after(:build) do |pic|
+      pic.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
+    end
+
   end
 end
