b2cda9d7b476b938fe7ea879443fdbea6393c16a
liz539z <abc11nu@gmail.com>
2020-10-16 08:33:47 +0900

    環境変数化したコントローラー

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,11 +1,40 @@
 class OrdersController < ApplicationController
 
   def index
- 
+    @item = Item.find(params[:item_id])
+    @order_form = OrderForm.new
   end
 
+
   def create
+ 
+    @order_form = OrderForm.new(order_params)
+    @item = Item.find(params[:item_id])
+
+    if @order_form.valid?
+      pay_item
+      
+      @order_form.save
+     return redirect_to root_path
+    else
+      render action: :index
+    end
   end
+
+private
+ def order_params
+  params.require(:order_form).permit(:postcode, :prefecture_id, :city, :address, :building, :phone_num).merge(token: params[:token], item_id: params[:item_id], user_id: current_user.id)
+ end
+
+  def pay_item
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+      Payjp::Charge.create(
+        amount: @item.price,
+        card: order_params[:token],
+        currency: 'jpy'
+      )
+  end
+
 
 
 end
