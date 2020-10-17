b7d4e092da1ba51571ecf431d395e72954e276ce
liz539z <abc11nu@gmail.com>
2020-10-17 10:57:17 +0900

    コントローラー修正
    before_actionでまとめ、
    不要なものは削除

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,9 +1,8 @@
 class OrdersController < ApplicationController
   before_action :authenticate_user!
+  before_action :set_item
   def index
-    @item = Item.find(params[:item_id])
     @order_form = OrderForm.new
-    order = Order.find(params[:item_id])
 
     # トップページへ
     if @item.order.present? || (user_signed_in? && current_user == @item.user)
@@ -11,13 +10,10 @@ class OrdersController < ApplicationController
     else
       @order_form
     end
-
-
   end
 
   def create
     @order_form = OrderForm.new(order_params)
-    @item = Item.find(params[:item_id])
 
     if @order_form.valid?
       pay_item
@@ -42,5 +38,9 @@ class OrdersController < ApplicationController
       card: order_params[:token],
       currency: 'jpy'
     )
+  end
+
+  def set_item
+    @item = Item.find(params[:item_id])
   end
 end
