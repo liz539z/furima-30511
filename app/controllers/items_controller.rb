0e66644b761a5af22798e04a89bac60eb6cfc790
liz539z <abc11nu@gmail.com>
2020-10-17 14:24:34 +0900

    コントローラー編集

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,6 @@
 class ItemsController < ApplicationController
+  before_action :authenticate_user!, only: :edit
+  before_action :set_item, only:[:show, :edit, :update]
   def index
     @items = Item.all.order('created_at DESC')
   end
@@ -21,12 +23,31 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
   end
 
+  def edit
+    if user_signed_in? && current_user == @item.user
+      render :edit
+    else
+      redirect_to root_path
+    end
+  end
+
+  def update
+    @item.update(item_params)
+    if @item.valid?
+      redirect_to item_path(@item.id)
+    else
+      render :edit
+    end
+  end
   private
 
   def item_params
     params.require(:item).permit(:name, :describe, :category_id, :condition_id, :postage_id, :prefecture_id, :delivery_day_id, :price, :image).merge(user_id: current_user.id)
+  end
+
+  def set_item
+    @item = Item.find(params[:id])
   end
 end
